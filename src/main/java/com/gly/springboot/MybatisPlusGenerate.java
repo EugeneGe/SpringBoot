a064de6649f432b7cb82c34bc46e2ca29aa12410
EugeneGe <geluyao@smartever.cn>
2022-08-13 10:29:22 +0800

    配置 MybatisPlusGenerate

--- src/main/java/com/gly/springboot/MybatisPlusGenerate.java
+++ src/main/java/com/gly/springboot/MybatisPlusGenerate.java
@@ -25,8 +25,8 @@ public class MybatisPlusGenerate {
     private static final String OUTPUT_DIR = "D://Java//Test//PlusCode";
     private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
     //-----------------常用变更----------------------
-    private static final String DATABASE_NAME = "data_test";
-    private static final String TABLE_NAME = "t_goods_barcode";
+    private static final String DATABASE_NAME = "data_basic";
+    private static final String TABLE_NAME = "sys_user";
     private static final Boolean SWAGGER_2 = true;
     //表名，多个英文逗号分割 和 tableName二选一配置
     private static final String SCANNER_TABLE_NAME = "";
